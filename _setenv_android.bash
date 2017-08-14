6b1bf16b94590e91fb89f1624d9892c10641a80b
licaon-kter <licaon.kter@protonmail.com>
2017-08-14 16:13:57 +0300

    Unbound 1.5.10 still works better, update to openssl 1.0.2l

--- _setenv_android.bash
+++ _setenv_android.bash
@@ -10,26 +10,26 @@ error ( ) {
         exit 1
 }
 
-export _UNBOUND_NAME=unbound-1.6.3
+export _UNBOUND_NAME=unbound-1.5.10
 export _LIBEVENT_NAME=libevent-2.0.22-cmake-configure-fixes
-export _OPENSSL_NAME=openssl-1.0.2k
+export _OPENSSL_NAME=openssl-1.0.2l
 export _EXPAT_NAME=expat-2.2.1
 export _LIBSODIUM_NAME=libsodium-master
 export _DNSCRYPTPROXY_NAME=dnscrypt-proxy-master
 
-export _UNBOUND_URL="https://unbound.net/downloads/unbound-1.6.3.tar.gz"
+export _UNBOUND_URL="https://unbound.net/downloads/unbound-1.5.10.tar.gz"
 export _LIBEVENT_URL="https://github.com/azat/libevent"
 export _LIBEVENT_GIT_BRANCH="cmake-configure-fixes"
 #export _LIBEVENT_PATCH="../libevent-2.0.22-stable.patch"
 export _LIBEVENT_PATCH=""
-export _OPENSSL_URL="https://www.openssl.org/source/openssl-1.0.2k.tar.gz"
+export _OPENSSL_URL="https://www.openssl.org/source/openssl-1.0.2l.tar.gz"
 export _EXPAT_URL="http://kent.dl.sourceforge.net/project/expat/expat/2.2.1/expat-2.2.1.tar.bz2"
 export _LIBSODIUM_URL="https://github.com/jedisct1/libsodium/archive/master.zip"
 export _DNSCRYPTPROXY_URL="https://github.com/jedisct1/dnscrypt-proxy/archive/master.zip"
 
-export _UNBOUND_URL_SIGNATURE="https://unbound.net/downloads/unbound-1.6.3.tar.gz.asc"
+export _UNBOUND_URL_SIGNATURE="https://unbound.net/downloads/unbound-1.5.10.tar.gz.asc"
 export _UNBOUND_URL_SIGNATURE_FINGERPRINT="EDFAA3F2CA4E6EB05681AF8E9F6F1C2D7E045F8D"
-export _OPENSSL_URL_SIGNATURE="https://www.openssl.org/source/openssl-1.0.2k.tar.gz.asc"
+export _OPENSSL_URL_SIGNATURE="https://www.openssl.org/source/openssl-1.0.2l.tar.gz.asc"
 export _OPENSSL_URL_SIGNATURE_FINGERPRINT="8657ABB260F056B1E5190839D9C4D26D0E604491"
 
 export ANDROID_NDK_HOME="$LOCAL_ANDROID_NDK_HOME"
